7ed60a5c502cb055facd46157ab0985d123416bb
Francisco <fco-jr@live.com>
2017-09-13 22:00:20 -0300

    contact page updated

--- js/javascript.js
+++ js/javascript.js
@@ -2,7 +2,5 @@ var enviar = document.querySelector("#enviar");
 enviar.addEventListener("click", function(event){
     event.preventDefault();
     alert("Message not sent, please contact by e-mail or phone.");
-    document.querySelector("#name").value="";
-    document.querySelector("#email").value="";
-    document.querySelector("#textarea").value="";
+    document.getElementById("form").reset();
 });
